8c7bc5899a89efe7108d2127c657acbec2b9ed2f
muhammadganiev <mukhammad.ganiev@mail.ru>
2023-07-08 20:58:15 +0400

    sad

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: muganiev <muganiev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 12:31:48 by muganiev          #+#    #+#             */
-/*   Updated: 2023/07/08 19:24:12 by muganiev         ###   ########.fr       */
+/*   Updated: 2023/07/08 20:56:31 by muganiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 
 int main(int ac, char **av)
 {
+    std::string args = av[0];
     if (ac == 1)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     else
     {
         for (int i = 1; i < ac; i++)
         {
-            for (int j = 0; j < strlen(av[i]); j++)
-                std::cout << (char)toupper(av[i][j]);
+            for (int j = 0; j < args.length(); j++)
+                std::cout << (char)std::toupper(av[i][j]);
         }
     }
 }
